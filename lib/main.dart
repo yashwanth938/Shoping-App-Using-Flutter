e777f6fb60310c0c20514b38c9276945418ee287
yashwanth938 <rongalayaswanth@gmail.com>
2022-10-04 18:55:29 +0530

    created some product classes and add to cart items

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 
 import 'app.dart';
+import 'model/app_state_model.dart';
+import 'model/products_repository.dart';
 
 void main() {
   return runApp(
-    const CupertinoStoreApp(),
+    ChangeNotifierProvider<AppStateModel>(
+      // NEW
+      create: (_) => AppStateModel()..loadProducts(), // NEW
+      child: const CupertinoStoreApp(), // NEW
+    ),
   );
 }
