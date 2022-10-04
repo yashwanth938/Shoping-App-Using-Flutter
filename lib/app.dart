9c5ba1d25728c37f902e2aaa83fd3e5e0c07d8da
yashwanth938 <rongalayaswanth@gmail.com>
2022-10-04 18:38:14 +0530

    added navaction bar

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,6 @@
+import 'package:cupertino_store/product_list_tab.dart';
+import 'package:cupertino_store/search_tab.dart';
+import 'package:cupertino_store/shoping_cart_tab.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
@@ -24,11 +27,56 @@ class CupertinoStoreHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text('Cupertino Store'),
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: 'Products',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.shopping_cart),
+            label: 'Cart',
+          ),
+        ],
       ),
-      child: SizedBox(),
+      tabBuilder: (context, index) {
+        late final CupertinoTabView returnValue;
+        switch (index) {
+          case 0:
+            returnValue = CupertinoTabView(
+              builder: (context) {
+                return const CupertinoPageScaffold(
+                  child: ProductListTab(),
+                );
+              },
+            );
+            break;
+          case 1:
+            returnValue = CupertinoTabView(
+              builder: (context) {
+                return const CupertinoPageScaffold(
+                  child: SearchTab(),
+                );
+              },
+            );
+            break;
+          case 2:
+            returnValue = CupertinoTabView(
+              builder: (context) {
+                return const CupertinoPageScaffold(
+                  child: ShoppingCartTab(),
+                );
+              },
+            );
+            break;
+        }
+        return returnValue;
+      },
     );
   }
 }
